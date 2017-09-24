0d93b99cc15172b5b09c14c647a7b0c65366c2eb
Paul-Kyle-Turner <turnerp5@students.rowan.edu>
2017-09-24 16:03:02 -0400

    delete database

--- paulturner/paulturner/settings.py
+++ paulturner/paulturner/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'paulturner.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.polls'),
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'ET'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
